d661758dda94ef631eed74f4e172496ca74d5ae7
Sreedhar <sreedhar85in@gmail.com>
2019-03-03 21:54:07 +0530

    commit on 3/3/2019 at 9:53pm

--- poojatest2proj/src/app/app.component.ts
+++ poojatest2proj/src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poojatest2proj';
+
+  
+  i = 0;
+  evenNumber = true;
+  numofbtnclicks = [0];
+
+  constructor(){}
+
+  Toggle()
+  {
+    this.i++;
+    this.numofbtnclicks.push(this.i);
+     console.log(this.numofbtnclicks);
+      if(this.i % 2 === 0)
+      {
+        this.evenNumber=true;
+      }
+      else 
+      {
+        this.evenNumber=false;
+      }
+      
+  }
+
+ 
+ 
+  
 }
