a8b0beb275aa223468a88ec3aca3afca06f426eb
Sreedhar S <Sreedhar.Sivansankaranarayanan1@T-Mobile.com>
2019-03-04 11:12:57 +0530

    commit at 03-04-2019-at 11-12AM

--- poojatest2proj/src/app/app.module.ts
+++ poojatest2proj/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FirstcompComponent } from './firstcomp/firstcomp.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstcompComponent
   ],
   imports: [
     BrowserModule
